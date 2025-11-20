5bf0549d90beaf4cfa1f173471d2c8259c601570
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2025-11-20 16:26:27 +0000

    conditional styling

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx  from  'clsx';
 
 export  enum  Status {
     Active = 'active',
@@ -10,17 +11,21 @@ export  enum  Status {
 export interface ActiveLabelProps {
   children: React.ReactNode;
   status: Status;
+  disabled?: boolean;
 }
 
-export default function StatusLabel({ children, status }: ActiveLabelProps) {
+export default function StatusLabel({ children, status,disabled }: ActiveLabelProps) {
     return <div
-        className={`inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium ${
-            status === Status.Active && 'bg-green-100 text-green-800' ||
-            status === Status.NotActive && 'bg-red-100 text-red-800' ||
-            status === Status.Pending && 'bg-orange-100 text-orange-800' ||
-            status === Status.Suspended && 'bg-blue-100 text-blue-800' ||
-            ''
-        }`}
+        className={clsx(
+            'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
+            status === Status.Active && 'bg-green-100 text-green-800',
+            status === Status.NotActive && 'bg-red-100 text-red-800',
+            status === Status.Pending && 'bg-orange-100 text-orange-800',
+            status === Status.Suspended && 'bg-blue-100 text-blue-800',
+            {
+                ['opacity-75 cursor-not-allowed']: disabled,
+            },
+        )}
     >
         <div className="w-1 h-1 mr-2 rounded-full bg-current"></div>
         {children}
